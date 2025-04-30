1f578f5ca93084e6034e531b0616eed35f060d70
tsviz <tsviz@github.com>
2025-04-30 10:01:28 -0500

    feat: add ESLint configuration and build scripts; generate output CSS with Tailwind

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+]);
